9d65f5a74ee69e2b410ebea95708e296a0a750a8
Aftab Ahmad <aftabah772@gmail.com>
2022-06-04 14:36:30 +0500

    Update settings.py

--- restapi/settings.py
+++ restapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g16ckk_hx23)stmf*e=ht@nt+r(c)!%$qa8zn##tg=vl447=='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
